b17b7d604195bb4ac2e66e0410dac4f533cdf5ce
Victorien Elvinger <victorien.elvinger@inria.fr>
2020-03-09 23:10:29 +0100

    feat: add guessing-game exercice

---
+++ corrections/01-guessing-game/guessing-game.c
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2020 Victorien Elvinger
+    Licensed under the zlib license (https://opensource.org/licenses/zlib).
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAX_NB 100
+#define DEBUG 0
+
+int main() {
+    int candidate;
+    int nb;
+    int iter = 0;
+
+    srand(time(NULL));
+    nb = rand() % (MAX_NB + 1);
+
+    if (DEBUG) {
+        printf("number to guess: %d\n", nb);
+    }
+
+    do {
+        printf("Nb ? ");
+        scanf("%d", &candidate);
+        if (candidate > nb) {
+            printf("Plus petit !\n");
+        }
+        else if (candidate < nb) {
+            printf("Plus grand !\n");
+        }
+        iter++;
+    } while (candidate != nb);
+
+    if (iter > 2) {
+        printf("Bravo ! Il t'as fallu tout de même %d tentatives !\n", iter);
+    }
+    else {
+        printf("Bravo !");
+    }
+
+    return 0;
+}
+
